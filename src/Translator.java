c2bde5666b53f34e3e8159cc900eb09c67c5bf5d
eisingvogel <selver981@gmail.com>
2017-02-24 12:58:45 -0600

    Removal of thread stuff
    WIP: Converted from thread class to standard class, imported Pi4J libraries, started work on serial communication

--- src/Translator.java
+++ src/Translator.java
@@ -1,30 +1,87 @@
-
 import java.util.BitSet;
+import java.io.IOException;
+import java.util.Date;
 
-public class Translator extends Thread{
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinDirection;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.io.gpio.event.GpioPinListener;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.event.PinEventType;
+
+import com.pi4j.io.serial.*;
+import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.util.Console;
+
+public class Translator{
 	
-	BitSet bitArray;
+
+	private BitSet bitArray;
+	final Serial serial;
 	
-	public Translator(BitSet incbitArray)
+	public Translator()
 	{
-		bitArray = incbitArray;
+
+		bitArray = new BitSet(9);
+		serial = SerialFactory.createInstance();
+		
+
 	}
 
 	private static BitSet fromString(final String s) {
 		return BitSet.valueOf(new long[]{Long.parseLong(s, 2)});
 	}
 	
-	public void run()
+	public void setArray(BitSet incbitArray)
+	{
+		//compare bitArrays here?
+		
+		bitArray = incbitArray;
+		translateArray();
+	}
+	
+	private void translateArray()
 	{
 		BitSet armArray = bitArray.get(0, 1);
 		BitSet leftArray = bitArray.get(1, 5);
 		BitSet rightArray = bitArray.get(5, 9);
 
-		//lock rs485
 
-		//transmit data
-
-		//unlock rs485
+		//translate here
+		
+		
+		
+		//end translation
+		
+		//pins 15 and 16 are the UART pins (GPIO 15 and 16)
+		
+		
+	}
+	
+	public void killTranslator() throws IllegalStateException,
+    IOException
+	{
+		try
+		{
+			serial.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	
