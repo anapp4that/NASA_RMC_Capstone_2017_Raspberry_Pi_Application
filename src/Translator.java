3f24287df4317f03308f8aff0755948cccaa2d99
incoruptable <zachbrandt51@yahoo.com>
2017-02-27 11:24:25 -0600

    Changes for translator to check and see if we have already sent this value over rs485.

--- src/Translator.java
+++ src/Translator.java
@@ -1,74 +1,76 @@
-import java.util.BitSet;
-import java.io.IOException;
-import java.util.Date;
-
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPin;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinDirection;
-import com.pi4j.io.gpio.PinMode;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
-import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
-import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
-import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
-import com.pi4j.io.gpio.event.GpioPinListener;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import com.pi4j.io.gpio.event.PinEventType;
-
 import com.pi4j.io.serial.*;
-import com.pi4j.util.CommandArgumentParser;
-import com.pi4j.util.Console;
+
+import java.io.IOException;
+import java.util.BitSet;
+import java.util.LinkedList;
+import java.util.List;
+
+
 
 public class Translator{
-	
 
-	private BitSet bitArray;
+
+	public static final int NUM_BYTES_TO_BE_SENT = 5;
 	final Serial serial;
+	byte[] previousSend;
+	private BitSet bitArray;
 	
 	public Translator()
 	{
 
 		bitArray = new BitSet(9);
 		serial = SerialFactory.createInstance();
-		
+		previousSend = new byte[NUM_BYTES_TO_BE_SENT];
 
 	}
 
 	private static BitSet fromString(final String s) {
 		return BitSet.valueOf(new long[]{Long.parseLong(s, 2)});
 	}
-	
-	public void setArray(BitSet incbitArray)
+
+	public void sendArray(BitSet incbitArray)
 	{
-		//compare bitArrays here?
-		
 		bitArray = incbitArray;
-		translateArray();
+		byte[] currentByteArray = translateArray();
+
+
+		//Compare bitArray's after translation.
+		List<byte> needsSending = new LinkedList<byte>();
+		for (int i = 0; i < NUM_BYTES_TO_BE_SENT; i++) {
+			if (currentByteArray[i] != previousSend[i]) {
+				needsSending.add(currentByteArray[i]);
+			}
+		}
+
+		if (needsSending.size() <= 0) {
+			return;
+		}
+
+
+		//Send the byteArray after we have verified if things are different or not.
+		previousSend = currentByteArray;
 	}
-	
-	private void translateArray()
+
+	private byte[] translateArray()
 	{
 		BitSet armArray = bitArray.get(0, 1);
 		BitSet leftArray = bitArray.get(1, 5);
 		BitSet rightArray = bitArray.get(5, 9);
 
+		byte[] translatedByteArray = new byte[NUM_BYTES_TO_BE_SENT];
 
 		//translate here
-		
-		
-		
-		//end translation
-		
-		//pins 15 and 16 are the UART pins (GPIO 15 and 16)
-		
-		
+		//translatedByteArray index meanings:
+		//index 0: front left motor  000Direction3SpeedBits
+		//index 1: back left motor   001Direction3SpeedBits
+		//index 2: front right motor 010Direction3SpeedBits
+		//index 3: back right motor  011Direction3SpeedBits
+		//index 4: excavation motor  100Direction3SpeedBits
+		//ANY FUTURE THINGS NEEDED TO BE DONE WILL HAVE TO BE APPENDED TO THIS BYTE ARRAY
+		//These are all subject to change once the EE's complete their design
+
+
+		//translation should result in an array of bytes. return these bytes to the sending function.
 	}
 	
 	public void killTranslator() throws IllegalStateException,
